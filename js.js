95d11b301e516d9ca577fc18315c9e88442f6140
Bhavishy rajak <bhavishyrajak2036@gmail.com>
2026-04-29 16:43:58 +0530

    Update js.js

--- js.js
+++ js.js
@@ -14,7 +14,7 @@ let isLoading = false;
 // =====================
 async function fetchMemes(count = 15) {
     try {
-       const res = await fetch(`https://nsfw1.p.rapidapi.com/nsfw /`)
+       const res = await fetch(`https://console.apify.com/storage/datasets`)
         const data = await res.json();
         memeQueue = [...memeQueue, ...data.memes.filter(m => m.url)]; // Valid URLs only
     } catch (err) {
